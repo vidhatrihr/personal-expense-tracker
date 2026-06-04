d63c7e82e1872444a319b096cba769d9c890d0aa
vidhatrihr <167363972+vidhatrihr@users.noreply.github.com>
2026-06-04 15:04:42 +0530

    Updated models.py

--- backend/models.py
+++ backend/models.py
@@ -5,33 +5,33 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String)
-    email = Column(String, unique=True)
-    password = Column(String)
 
-    # relationships
-    budget = relationship('Budget', back_populates='user', uselist=False)
-    expenses = relationship('Expense', back_populates='user')
+class User(db.Model, UserMixin):
+  __tablename__ = 'users'
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  name = Column(String)
+  email = Column(String, unique=True)
+  password = Column(String)
+
+  budget = relationship('Budget', back_populates='user', uselist=False)
+  expenses = relationship('Expense', back_populates='user')
 
 
 class Budget(db.Model):
-    __tablename__ = 'budgets'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    amount = Column(Float, default=0)
-    user_id = Column(Integer, ForeignKey('users.id'))
+  __tablename__ = 'budgets'
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  amount = Column(Float, default=0)
+  user_id = Column(Integer, ForeignKey('users.id'))
 
-    user = relationship('User', back_populates='budget')
+  user = relationship('User', back_populates='budget')
 
 
 class Expense(db.Model):
-    __tablename__ = 'expenses'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    amount = Column(Float)
-    category = Column(String)
-    description = Column(String)
-    user_id = Column(Integer, ForeignKey('users.id'))
+  __tablename__ = 'expenses'
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  amount = Column(Float)
+  category = Column(String)
+  description = Column(String)
+  user_id = Column(Integer, ForeignKey('users.id'))
 
-    user = relationship('User', back_populates='expenses')
+  user = relationship('User', back_populates='expenses')
